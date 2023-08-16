c0210be3a49be1405fa30920120b2733af006ad8
Abhinav Chaturvedi <achaturvedi@calance.com>
2023-08-16 13:15:59 +0530

    Add commands to list all instances, show public ip of given instance amd start/stop a instance

--- README.md
+++ README.md
@@ -34,15 +34,29 @@ poetry install
 To run the CLI tool and start or stop an EC2 instance, use the following command:
 
 ```bash
-poetry run start-stop-instances instance_id
+poetry run start-stop-instances show-instances
+poetry run start-stop-instances start-stop instance_id
 ```
 or
 
 ```bash
-python start_stop_instances/start_stop_instances.py instance_id
+python start_stop_instances/start_stop_instances.py show-instances
+python start_stop_instances/start_stop_instances.py start-stop instance_id
 ```
 
 Replace instance_id with the actual EC2 instance ID you want to start or stop.
+
+For any help,
+
+```bash
+poetry run start-stop-instances --help
+```
+
+or 
+
+```bash
+python start_stop_instances/start_stop_instances.py --help
+```
 
 ## AWS Credentials and Environment Variables
 Make sure to configure your AWS credentials using environment variables or the AWS CLI. Additionally, you can place your AWS credentials and other environment variables in a .env file in the project root directory. An example .env file:
--- start_stop_instances/start_stop_instances.py
+++ start_stop_instances/start_stop_instances.py
@@ -4,6 +4,7 @@ import boto3
 from botocore.exceptions import WaiterError
 from dotenv import load_dotenv
 import os
+from tabulate import tabulate
 
 # Get the path to the directory containing this script
 script_directory = Path(__file__).resolve().parent.parent
@@ -12,6 +13,13 @@ load_dotenv(script_directory / ".env")  # Load environment variables from .env f
 
 
 app = typer.Typer()
+
+# Load AWS credentials from environment variables
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+# DEPRECATED:: aws_session_token
+# aws_session_token = os.getenv("AWS_SESSION_TOKEN")
+aws_region = os.getenv("AWS_DEFAULT_REGION")
 
 
 def wait_for_instance_state(ec2, instance_id, desired_state):
@@ -24,18 +32,10 @@ def wait_for_instance_state(ec2, instance_id, desired_state):
 
 
 @app.command()
-def main(instance_id: str):
+def show_instances():
     """
-    Start or stop an EC2 instance.
+    Display all EC2 instances. Show Instance Id, Instance Name and Instance Status
     """
-    # Load AWS credentials from environment variables
-    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-    # DEPRECATED:: aws_session_token
-    # aws_session_token = os.getenv("AWS_SESSION_TOKEN")
-    aws_region = os.getenv("AWS_DEFAULT_REGION")
-
-    # print(aws_access_key_id)
 
     # Create an EC2 client
     ec2 = boto3.client(
@@ -48,13 +48,55 @@ def main(instance_id: str):
 
     # Check if AWS credentials are configured
     try:
-        ec2.describe_instances()
+        _instances = ec2.describe_instances()
+        if "Reservations" in _instances and len(_instances["Reservations"]) > 0:
+            # Get instance Ids and instance_name for all the instances
+            _instance_ids = [
+                i["Instances"][0]["InstanceId"] for i in _instances["Reservations"]
+            ]
+            _instance_names = [
+                i["Instances"][0]["Tags"][0]["Value"]
+                for i in _instances["Reservations"]
+                if len(i["Instances"][0]["Tags"]) > 0
+            ]
+            _instance_states = [
+                i["Instances"][0]["State"]["Name"] for i in _instances["Reservations"]
+            ]
+
+            if len(_instance_ids) == len(_instance_names) == len(_instance_states):
+                _instance_metadata = [
+                    [id, name, state]
+                    for id, name, state in zip(
+                        _instance_ids, _instance_names, _instance_states
+                    )
+                ]
+                headers = ["InstanceId", "InstanceName", "InstanceState"]
+                table = tabulate(_instance_metadata, headers=headers, tablefmt="grid")
+                typer.echo(table)
+        else:
+            typer.echo("No instances configured for this account.")
+            return
     except Exception as e:
         print(str(e))
         typer.echo(
             "AWS credentials are not configured. Please configure them using AWS CLI or environment variables."
         )
         return
+
+
+@app.command()
+def start_stop(instance_id: str):
+    """
+    Start or stop an EC2 instance.
+    """
+    # Create an EC2 client
+    ec2 = boto3.client(
+        "ec2",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        # aws_session_token=aws_session_token,
+        region_name=aws_region,
+    )
 
     # Prompt for action
     action = typer.prompt("Enter 'start' or 'stop' to perform the action")
@@ -77,6 +119,36 @@ def main(instance_id: str):
         typer.echo(f"Instance {instance_id} stopped successfully")
     else:
         typer.echo("Invalid action. Use 'start' or 'stop'")
+
+
+@app.command()
+def show_instance_ip(instance_id: str):
+    """Get Public ip of the instance
+
+    Args:
+        instance_id (str): EC2 Instance ID
+    """
+
+    # Create an EC2 client
+    ec2 = boto3.client(
+        "ec2",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        # aws_session_token=aws_session_token,
+        region_name=aws_region,
+    )
+
+    # Check if AWS credentials are configured
+    try:
+        response = ec2.describe_instances(InstanceIds=[instance_id])
+        public_ip = response["Reservations"][0]["Instances"][0]["PublicIpAddress"]
+        typer.echo(f"Public IP address: {public_ip}")
+    except Exception as e:
+        print(str(e))
+        typer.echo(
+            "AWS credentials are not configured. Please configure them using AWS CLI or environment variables."
+        )
+        return
 
 
 if __name__ == "__main__":
